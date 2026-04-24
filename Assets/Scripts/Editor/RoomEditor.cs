6e0927ad7ad597adc138ea113fe75933f384b0c1
Ekipogh <ekipogh@mail.ru>
2026-04-24 16:10:56 +0100

    Mansion greyboxing
    - Made simple mansion layot in greyboxing
    - Added PolygonPrototype package

---
+++ Assets/Scripts/Editor/RoomEditor.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Room))]
+public class RoomEditor : Editor
+{
+    Room room;
+
+    void OnEnable()
+    {
+        room = (Room)target;
+    }
+
+    void ToggleWall(GameObject wall, GameObject door)
+    {
+        bool isActive = wall.activeSelf;
+        wall.SetActive(!isActive);
+        door.SetActive(isActive);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Toggle North"))
+        {
+            ToggleWall(room.northWall, room.northDoor);
+        }
+        if (GUILayout.Button("Toggle South"))
+        {
+            ToggleWall(room.southWall, room.southDoor);
+        }
+        if (GUILayout.Button("Toggle East"))
+        {
+            ToggleWall(room.eastWall, room.eastDoor);
+        }
+        if (GUILayout.Button("Toggle West"))
+        {
+            ToggleWall(room.westWall, room.westDoor);
+        }
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject northWall;
+    public GameObject northDoor;
+    public GameObject southWall;
+    public GameObject southDoor;
+    public GameObject eastWall;
+    public GameObject eastDoor;
+    public GameObject westWall;
+    public GameObject westDoor;
+}
